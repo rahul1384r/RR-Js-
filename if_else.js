d80dbae961885cfa0ca52e72290dc8cb2ef3f5c2
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-01-30 05:25:05 +0530

    js

---
+++ if_else.js
@@ -0,0 +1,18 @@
+// let a="2";
+// // losse comparison only checks the value not type
+// console.log(a==2);
+// console.log(a!=2);
+// // strict comparison checks the value as well as type
+// console.log(a===2);
+// console.log(a!==2);
+
+
+
+a=2;
+let b=7;
+if(a==b){
+    console.log("a is equal to b");
+}
+else{
+    console.log("a is not equal to b");
+}
