59eff1370ce2b121b1e35afef5e0f0bbf9beff47
rahul1384r <156775643+rahul1384r@users.noreply.github.com>
2024-03-17 22:29:37 +0530

    s

--- codeBlock.js
+++ codeBlock.js
@@ -15,11 +15,18 @@
 // }
 // console.log(year,month);      //output--->2004
 
-let year=2004;
-if(true){
-    let year=2005;
-    let month=7;    //it'll not get accessed from outside the block.
-    var month=7;    // it'll get accessed from outside the block
-    console.log(year);
+// let year=2004;
+// if(true){
+//     let year=2005;
+//     let month=7;    //it'll not get accessed from outside the block.
+//     var month=7;    // it'll get accessed from outside the block
+//     console.log(year);
+// }
+// console.log(year,month);
+
+
+var x=100;
+{
+    var x=10;
 }
-console.log(year,month);
+console.log(x)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+var x=100;
+{
+    var x=10;
+}
+console.log(x)
